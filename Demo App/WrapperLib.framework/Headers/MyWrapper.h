09a58d1a5e85a1023df1bd4129b814a0d0a28145
robwills <rob.wills@gmail.com>
2011-05-17 10:03:29 -0700

    Add a local built copy of the WrapperLib framework to the Demo App project, plus a json file to read

---
+++ Demo App/WrapperLib.framework/Headers/MyWrapper.h
@@ -0,0 +1,17 @@
+//
+//  MyWrapper.h
+//  WrapperLibrary
+//
+//  Created by Rob Wills on 5/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MyWrapper : NSObject {
+@private
+    
+}
+- (NSString *)readName:(NSData *)jsonData;
+@end
